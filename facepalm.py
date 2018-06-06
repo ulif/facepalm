2e6e0dd274b524ff2546ab23fd6cd5a7f52138b1
ulif <uli@gnufix.de>
2018-06-06 22:39:41 +0200

    Try to find faces, finally.

--- facepalm.py
+++ facepalm.py
@@ -2,4 +2,17 @@
 # facepalm.py
 #
 # detects faces in images.
+#
+from PIL import Image
+import face_recognition
+
+# Read image
+image = face_recognition.load_image_file("obama_and_biden.jpg")
+
+
+face_locs = face_recognition.face_locations(
+        image,
+        number_of_times_to_upsample=0, model="cnn")
+
+print("Found %s faces" % len(face_locs))
 
