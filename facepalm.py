50ebc126386cf7a2468c5b7ffce75aae8bb14483
ulif <uli@gnufix.de>
2018-06-08 01:47:43 +0200

    Draw rectangle and show picture.

--- facepalm.py
+++ facepalm.py
@@ -3,11 +3,12 @@
 #
 # detects faces in images.
 #
-from PIL import Image
+from PIL import Image, ImageDraw
 import face_recognition
 
 # Read image
 image = face_recognition.load_image_file("obama_and_biden.jpg")
+pil_image = Image.fromarray(image)
 
 
 face_locs = face_recognition.face_locations(
@@ -15,6 +16,13 @@ face_locs = face_recognition.face_locations(
         number_of_times_to_upsample=0, model="cnn")
 
 print("Found %s faces" % len(face_locs))
+drawing = ImageDraw.Draw(pil_image)
 for (top, right, bottom, left) in face_locs:
     print(top, right, bottom, left)
+    drawing.rectangle(
+        ((left, top), (right, bottom)), outline=(0x00, 0x00, 0xff))
+
+del drawing
+
+pil_image.show()
 
