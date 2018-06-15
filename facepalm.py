45ce9b385411dd013c6c448ff1f4d4e893cd5192
ulif <uli@gnufix.de>
2018-06-15 03:38:42 +0200

    Move function.

--- facepalm.py
+++ facepalm.py
@@ -6,6 +6,15 @@
 from PIL import Image, ImageDraw, ImageFilter
 import face_recognition
 
+def pixelate(im, box):
+    im_w, im_h = im.size
+    y0, x1, y1, x0 = box
+    w, h = x1 - x0, y1 - y0
+    snippet = im.crop((x0, y0, x1, y1), )
+    snippet = snippet.resize((int(w/15.0), int(h/15.0)), )
+    snippet = snippet.resize((w, h), )
+    im.paste(snippet, box=(x0, y0))
+
 # Read image
 image = face_recognition.load_image_file("obama_and_biden.jpg")
 pil_image = Image.fromarray(image)
@@ -24,15 +33,6 @@ for (top, right, bottom, left) in face_locs:
         outline=(0x00, 0x00, 0xff))
 
 del drawing
-
-def pixelate(im, box):
-    im_w, im_h = im.size
-    y0, x1, y1, x0 = box
-    w, h = x1 - x0, y1 - y0
-    snippet = im.crop((x0, y0, x1, y1), )
-    snippet = snippet.resize((int(w/15.0), int(h/15.0)), )
-    snippet = snippet.resize((w, h), )
-    im.paste(snippet, box=(x0, y0))
 
 pixelate(pil_image, face_locs[0])
 pil_image.show()
